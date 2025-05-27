bddc93c9a399cc41b3b0523b5300e3023addef8c
leen2233 <arslanrejepow223@gmail.com>
2025-05-27 11:31:59 +0800

    Add user id field and improve User class functionality

--- lib/db.py
+++ lib/db.py
@@ -1,16 +1,22 @@
 from dataclasses import asdict, dataclass
 from typing import Dict, Optional
+
 from pymongo import MongoClient
 
 client = MongoClient()
 db = client.chat
+
 
 @dataclass
 class User:
     username: str
     email: str
     password: str
+    _id: Optional[str] = None
     full_name: Optional[str] = None
+
+    def __repr__(self) -> str:
+        return f"<{self._id} - {self.username}>"
 
 
 def get_user_by_username(username) -> Optional[User]:
@@ -19,12 +25,18 @@ def get_user_by_username(username) -> Optional[User]:
         return User(**item)
     return None
 
+
 def check_user_exists(username: Optional[str] = None, email: Optional[str] = None) -> Dict[str, bool]:
     username_exists = db.users.find_one({"username": username}) is not None
     email_exists = db.users.find_one({"username": username}) is not None
 
     return {"username": username_exists, "email": email_exists}
 
+
 def create_user(user: User):
-    item = db.users.insert_one(asdict(user))
+    data = asdict(user)
+    data.pop("_id")  # let mongodb assign random id
+    item = db.users.insert_one(data)
+    user._id = item.inserted_id
+    print(user)
     return user
